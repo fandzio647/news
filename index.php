1ccd5b83c26ca61b73b83c23f5de478e65fb956a
Maksio0o <90030598+Maksio0o@users.noreply.github.com>
2023-11-28 22:58:09 +0100

    title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $wyn_wpis=0;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styl.css">
-    <title>Wpisy</title>
+    <title>SWIEZENEWSY.PL</title>
     <link rel="icon" type="image/x-icon" href="logo.ico">
 </head>
 
