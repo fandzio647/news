ac30c06b39e2ffdfb082d1d72aa6ecbf06cdc533
Maksio0o <90030598+Maksio0o@users.noreply.github.com>
2023-11-28 22:59:28 +0100

    TITLE

--- wpis.php
+++ wpis.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styl.css">
-    <title>Wpisy</title>
+    <title>NEWS</title>
     <link rel="icon" type="image/x-icon" href="logo.ico">
 </head>
 
